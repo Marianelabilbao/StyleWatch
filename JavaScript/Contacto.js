21bba198c059de59c0dad1b22167b2a4b112ee51
Marianelabilbao <167768036+Marianelabilbao@users.noreply.github.com>
2024-05-27 10:11:24 -0300

    Update Contacto.js
    Verificar que el usuario elija un medio de contacto y que ese medio de contacto no este vacio

--- JavaScript/Contacto.js
+++ JavaScript/Contacto.js
@@ -1,9 +1,33 @@
+//DOM  Asegura que todo este cargado previo a ejecutarse
 document.addEventListener("DOMContentLoaded", function() {
+    //que elementos se van a manipular
     const form = document.getElementById("contactForm");
+    const prefEmail = document.getElementById("prefEmail");
+    const prefTelefono = document.getElementById("prefTelefono");
+    const email = document.getElementById("email");
+    const telefono = document.getElementById("telefono");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault(); // Evitar que el formulario se envíe automáticamente
 
+        // Agregar eventos a las casillas de verificación, para que sea requerido uno de los dos medios de contacto
+    prefEmail.addEventListener("change", updateRequiredFields);
+    prefTelefono.addEventListener("change", updateRequiredFields);
+
+         // Función para actualizar los atributos required, para que el usuario no deje el medio elegido en blanco
+    function updateRequiredFields() {
+        if (prefEmail.checked) {
+            email.required = true;
+        } else {
+            email.required = false;
+        }
+
+        if (prefTelefono.checked) {
+            telefono.required = true;
+        } else {
+            telefono.required = false;
+        }
+    }
         // Validar los campos del formulario
         const nombre = document.getElementById("nombre").value.trim();
         const email = document.getElementById("email").value.trim();
@@ -11,13 +35,15 @@ document.addEventListener("DOMContentLoaded", function() {
         const mensaje = document.getElementById("mensaje").value.trim();
 
         // Validar que los campos obligatorios no estén vacíos
-        if (nombre === "" || email === "" || motivo === "" || mensaje === "") {
+        if (!nombre === "" || !email === "" || !motivo === "" || !mensaje === "") {
+            Alert("Por favor, complete todos los campos obligatorios.")
             return;
         }
 
         // Validar el formato del email utilizando una expresión regular simple
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)){
+            alert("Por favor, ingrese un correo electrónico válido.")
             return;
         }
 
