40f0004f6a530c21d6ad9320fc49ac65bad4ad1c
andreSe85 <oa.gcia@gmail.com>
2024-05-28 18:32:02 -0300

    Se quitaron los comentarios

--- JavaScript/Contacto.js
+++ JavaScript/Contacto.js
@@ -1,6 +1,5 @@
-// DOM - Asegura que todo esté cargado previo a ejecutarse
+
 document.addEventListener("DOMContentLoaded", function() {
-    //que elementos se van a manipular
     const form = document.getElementById("contactForm");
     const prefEmail = document.getElementById("prefEmail");
     const prefTelefono = document.getElementById("prefTelefono");
@@ -8,9 +7,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const telefono = document.getElementById("telefono");
 
     form.addEventListener("submit", function(event) {
-        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+        event.preventDefault();
 
-        // Función para actualizar los atributos required, para que el usuario no deje el medio elegido en blanco
         function updateRequiredFields() {
             if (prefEmail.checked) {
                 email.required = true;
@@ -24,38 +22,32 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
 
-        // Agregar eventos a las casillas de verificación, para que sea requerido uno de los dos medios de contacto
         prefEmail.addEventListener("change", updateRequiredFields);
         prefTelefono.addEventListener("change", updateRequiredFields);
 
-        // Validar los campos del formulario
         const nombre = document.getElementById("nombre").value.trim();
         const email = document.getElementById("email").value.trim();
         const telefono = document.getElementById("telefono").value.trim();
         const motivo = document.getElementById("motivo").value;
         const mensaje = document.getElementById("mensaje").value.trim();
 
-        // Validar que los campos obligatorios no estén vacíos
         if (nombre === "" || motivo === "" || mensaje === "") {
             alert("Por favor, complete todos los campos obligatorios.");
             return;
         }
 
-        // Validar el formato del email utilizando una expresión regular simple
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email)){
             alert("Por favor, ingrese un correo electrónico válido.");
             return;
         }
 
-        // Validar que al menos un medio de contacto esté seleccionado
         if (!prefEmail.checked && !prefTelefono.checked) {
             alert("Por favor, seleccione al menos un medio de contacto: email o teléfono.");
             return;
         }
 
-        // Si todos los campos son válidos, enviar el formulario (aquí puedes agregar el código para enviar el formulario a tu backend si lo tienes)
         alert("Formulario enviado correctamente. ¡Gracias por contactar a StyleWatch!");
-        form.reset(); // Limpiar el formulario después de enviarlo
+        form.reset();
     });
 });
--- JavaScript/pokemonscript.js
+++ JavaScript/pokemonscript.js
@@ -1,8 +1,7 @@
-// Consumiendo la API de Pokémon
+
 fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
 .then(response => response.json())
 .then(data => {
-    // Procesamiento de la información que llega de la API
     console.log(data);
     const pokemonSelect = document.getElementById('pokemon-select');
     data.results.forEach(pokemon => {
@@ -14,31 +13,27 @@ fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
 })
 .catch(error => console.log("Ocurrió un error al obtener la lista de Pokémon:", error));
 
-// Función para mostrar el Pokémon seleccionado
 function showSelectedPokemon() {
     const pokemonSelect = document.getElementById('pokemon-select');
     const pokemonUrl = pokemonSelect.value;
     fetch(pokemonUrl)
     .then(response => response.json())
     .then(data => {
-        // Procesamiento de la información que llega de la API
         console.log(data);
         const pokemonGrid = document.querySelector('.pokemon-grid');
-        pokemonGrid.innerHTML = ''; // Limpiar la malla de imágenes antes de mostrar el nuevo Pokémon
+        pokemonGrid.innerHTML = '';
         const imageElement = document.createElement('img');
         imageElement.src = data.sprites.front_default;
         pokemonGrid.appendChild(imageElement);
     })
     .catch(error => console.error("Ocurrió un error al obtener el Pokémon:", error));
 }
-// script.js
+
 document.addEventListener("DOMContentLoaded", function() {
-    // Esperar 5 segundos
     setTimeout(function() {
-        // Obtener el iframe del video
         var iframe = document.getElementById('video');
         var src = iframe.src;
         iframe.src = src + (src.indexOf('?') > -1 ? '&' : '?') + 'autoplay=1';
-    }, 1000); // 1000 milisegundos = 1 segundos
+    }, 1000);
 });
 
